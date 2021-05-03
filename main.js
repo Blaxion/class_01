84ac94009b34dcf26d1920ad4b9faa123159604d
Blaxion <khibaziev.f@gmail.com>
2021-05-03 11:00:04 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// ## EXO1
+// ### Créer une class Personnage avec comme propriétés : 
+// ### age , nom , ville
+// ### Faire 2 instances de cette class.
+
+class Personnage {
+    constructor (age , nom , ville) {
+        this.age = age
+        this.nom = nom
+        this.ville = ville
+    }
+    sePrésenter(){
+        console.log(`Bonjour, je m'apelle ${this.nom}`)
+    }
+}
+
+let personnage1 = new Personnage (18 , "LeChat", "Bxl")
+let personnage2 = new Personnage (26 , "Farhad", "Bxl")
+
+// ## EXO2
+// ### Rajouter la methode 'sePresenter' à la class Personnage qui dira "Bonjour, je m'appelle [nom-de-la-personne] !" via un console.log
+// ### Lancer cette méthode sur les deux instances de l'exo1
+
+personnage1.sePrésenter()
+personnage2.sePrésenter()
